0e3bcf5d9c8f68c9cd2356cc074e5da4c058cdb2
sunxiao <297980434@qq.com>
2020-07-20 19:03:21 +0800

    jieba分词

---
+++ com/learn/python/socket/SocketIp.py
@@ -0,0 +1,21 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('www.sina.com.cn', 80))
+
+# 发送数据:
+s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
+
+# 接收数据:
+buffer = []
+while True:
+    # 每次最多接收1k字节:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = b''.join(buffer)
+
+# 关闭连接:
+s.close()
